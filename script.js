8bc174be98c88114fa77b32afa04e947315b5242
baranilakshmi <baranilakshmim@gmail.com>
2021-05-31 19:54:54 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,78 @@
+console.log("baranilakshmi");
+
+console.log(7+true);
+
+let x, y;
+x = 100, y = 500;
+
+let z = x + y;
+
+console.log(z);
+console.log(x + y);
+console.log("x");
+console.log(x);
+
+console.log(x+true);
+console.log(y-false);
+console.log("x" == "x");
+console.log(x == x);
+
+let q = 10, _q = 20, $q = 30, q1 = 40;
+
+console.log(q+_q);
+console.log(q+_q+$q+q1);
+console.log(q*q1);
+console.log(_q/q);
+
+console.log(x = x);
+console.log(2 === 2);
+console.log("2" === "2");
+console.log(2 === "2");
+console.log(2 == 2);
+console.log("2" == "2");
+console.log(2 == "2");
+
+const p = 10;
+console.log(p);
+
+//let p = 8;
+
+let a = 5, b = 5, c = 5;
+
+console.log(a*b*c);
+console.log(a*b/c);
+
+console.log(2+2);
+console.log(2-2);
+console.log("2" + "2");
+console.log("2" - "2");
+console.log(5+"5");
+console.log(5-"5");
+
+const name = "dhoni";
+const fullname = "mahendra singh dhoni";
+
+console.log(name);
+console.log(fullname);
+
+console.log(`${name} ${fullname}`);
+
+console.log('bat' - 'ball');
+console.log(76-null);
+
+const type ="good";
+console.log(typeof(type));
+
+const body = true;
+console.log(typeof(body));
+
+console.log(Number("10"));
+console.log(parseInt("7"));
+
+console.log("r"+"5");
+
+
+
+
+
+
